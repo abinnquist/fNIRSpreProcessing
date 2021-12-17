5457f978a12d88260e1ad0511264b56faa55024b
abinnquist <57020313+abinnquist@users.noreply.github.com>
2021-12-16 18:05:27 -0800

    Update fNIRSFilterPipeline.m

--- fNIRSFilterPipeline.m
+++ fNIRSFilterPipeline.m
@@ -2,22 +2,28 @@
 %step 4 - our filtering pipeline
 %-----------------------------------------------
  
-function [dconverted dnormed]= fNIRSFilterPipeline(d, SD, samprate)
+function [dconverted, dnormed]= fNIRSFilterPipeline(d, SD, samprate, motionCorr)
     %depends on Homer2 package
     warning off; %sometimes hmrIntensity2Conc gives a warning we don't care about here
     %see hmrMotionArtifact in Homer2 documentation for parameter description
-    [~,tIncCh] = hmrMotionArtifactByChannel(d, samprate, SD, ones(length(d),1), 1, 1, 5, 2);
+    [tInc,tIncCh] = hmrMotionArtifactByChannel(d, samprate, SD, ones(length(d),1), 0.5, 0.5, 5, 2);
     
-    %4/21/2020
-    %Homer2 motionPCA filtering has not worked very well on our recent
-    %data, so reverting to a simpler method of correction for
-    %discontinutites and changes in volatility, and then generating report
-    %for which channels might still have problematic artifacts after processing
-    %while I work on learning Homer3. Blog description coming soon.
-    dfiltered = BaselineVolatilityCorrection(d, samprate, SD, tIncCh);
-
+    if any(or(motionCorr==0,motionCorr==2))
+        %4/21/2020
+        dfiltered = BaselineVolatilityCorrection(d, samprate, SD, tIncCh);
+    end
+    
+    if any(or(motionCorr==1,motionCorr==3))
+        [dfiltered,~,~] = hmrMotionCorrectPCA_Ch(SD, d, tInc, tIncCh, .8);
+    end
+    
     %see hmrIntensity2Conc in Homer2 documentation for parameter description
     [dconverted, ~] = hmrIntensity2Conc(dfiltered, SD, samprate, 0.008, 0.2, [6, 6]);
+    
+    if any(or(motionCorr==2,motionCorr==3))
+        dconverted = hmrMotionCorrectCbsi(dconverted,SD);
+    end
+    
     dnormed = zscore(dconverted);
     warning on;
 end
