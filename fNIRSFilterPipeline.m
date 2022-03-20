b1111d819d770cd3e4fb36e5d5b098fdb7d37b3a
abinnquist <57020313+abinnquist@users.noreply.github.com>
2022-03-19 18:41:23 -0700

    Update fNIRSFilterPipeline.m

--- fNIRSFilterPipeline.m
+++ fNIRSFilterPipeline.m
@@ -8,19 +8,22 @@ function [dconverted, dnormed]= fNIRSFilterPipeline(d, SD, samprate, motionCorr)
     %see hmrMotionArtifact in Homer2 documentation for parameter description
     [tInc,tIncCh] = hmrMotionArtifactByChannel(d, samprate, SD, ones(length(d),1), 0.5, 0.5, 5, 2);
     
-    if any(or(motionCorr==0,motionCorr==2))
+    if any(or(motionCorr==1,motionCorr==3))
         %4/21/2020
         dfiltered = BaselineVolatilityCorrection(d, samprate, SD, tIncCh);
     end
     
-    if any(or(motionCorr==1,motionCorr==3))
+    if motionCorr==2
         [dfiltered,~,~] = hmrMotionCorrectPCA_Ch(SD, d, tInc, tIncCh, .8);
     end
     
+    if any(or(motionCorr==0,motionCorr==4))
+        dfiltered=d;
+    end
     %see hmrIntensity2Conc in Homer2 documentation for parameter description
     [dconverted, ~] = hmrIntensity2Conc(dfiltered, SD, samprate, 0.008, 0.2, [6, 6]);
     
-    if any(or(motionCorr==2,motionCorr==3))
+    if any(or(motionCorr==3,motionCorr==4))
         dconverted = hmrMotionCorrectCbsi(dconverted,SD);
     end
     
