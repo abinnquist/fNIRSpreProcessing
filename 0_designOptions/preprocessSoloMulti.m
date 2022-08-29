cd54d47fc1d6f419af6814a3a7f1261763837772
abinnquist <57020313+abinnquist@users.noreply.github.com>
2022-08-29 16:52:14 -0700

    Update preprocessSoloMulti.m

--- 0_designOptions/preprocessSoloMulti.m
+++ 0_designOptions/preprocessSoloMulti.m
@@ -62,9 +62,14 @@ for i=1:length(currdir)
                 onset = trigInfo.Onset;
                 s(find(t==onset(1)),1) = 1;
             end
-            digfile = strcat(scanfolder,filesep,'digpts.txt');
-            if device>=2 && exist(digfile,'file')
-                mni_ch_table = getMNIcoords(digfile, SD);
+            if SD.SrcPos==0
+                load(strcat(rawdir,filesep,'SD_fix.mat'))
+                digfile = strcat(rawdir,filesep,'digpts.txt');
+            else
+                digfile = strcat(scanfolder,filesep,'digpts.txt');
+                if device>=2 && exist(digfile,'file')
+                    mni_ch_table = getMNIcoords(digfile, SD);
+                end
             end
 
             %2) Trim beginning of data or trim beginning and end off
