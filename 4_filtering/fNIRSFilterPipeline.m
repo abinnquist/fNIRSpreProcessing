743b8f5de09ff0aeae97f6909a7ab506be82a60c
abinnquist <57020313+abinnquist@users.noreply.github.com>
2022-09-20 14:39:51 -0700

    Update fNIRSFilterPipeline.m

--- 4_filtering/fNIRSFilterPipeline.m
+++ 4_filtering/fNIRSFilterPipeline.m
@@ -15,15 +15,13 @@ function [dconverted, dnormed]= fNIRSFilterPipeline(d, SD, samprate, motionCorr,
     
     if motionCorr==2
         dod = hmrIntensity2OD( d );
-        if ~isempty(coords)
-            dod = spatialPCFilter(dod, coords);
-        end
+        addpath(genpath('homer2')); %must be in the pwd to work
+        [dod] = hmrMotionCorrectWavelet(dod,SD,1.5);
+%         if ~isempty(coords)
+%             dod = spatialPCFilter(dod, coords);
+%         end
         dod = hmrBandpassFilt(dod,samprate,0.008,0.2);
         ppf = [6 6];
-        if length(ppf)~=length(SD.Lambda)
-            disp( sprintf('Warning: ppf must be a vector with the same number of elements as SD.Lambda\n    Using default values of [6 6]') );
-            ppf = [6 6];
-        end
         dconverted = hmrOD2Conc( dod, SD, ppf );
     end
     
