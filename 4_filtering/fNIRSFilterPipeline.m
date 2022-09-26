7c04b6d9d068f3a0d03b3866b507a28eb5266810
abinnquist <57020313+abinnquist@users.noreply.github.com>
2022-09-25 17:46:36 -0700

    Update fNIRSFilterPipeline.m

--- 4_filtering/fNIRSFilterPipeline.m
+++ 4_filtering/fNIRSFilterPipeline.m
@@ -8,30 +8,36 @@ function [dconverted, dnormed]= fNIRSFilterPipeline(d, SD, samprate, motionCorr,
     %see hmrMotionArtifact in Homer2 documentation for parameter description
     
     if any(or(motionCorr==1,motionCorr==3))
-        [~,tIncCh] = hmrMotionArtifactByChannel(d, samprate, SD, ones(length(d),1), 1, 1, 5, 2);
-        dfiltered = BaselineVolatilityCorrection(d, samprate, SD, tIncCh);
+        [tInc,tIncCh] = hmrMotionArtifactByChannel(d, samprate, SD, ones(length(d),1), 1, 1, 5, 2);
+        if motionCorr==1
+            dfiltered = BaselineVolatilityCorrection(d, samprate, SD, tIncCh);
+        else
+            [dfiltered,~,~] = hmrMotionCorrectPCA_Ch( SD, d, tInc, tIncCh, 0.9);
+        end
         [dconverted, ~] = hmrIntensity2Conc(dfiltered, SD, samprate, 0.008, 0.2, [6, 6]);
     end
     
-    if motionCorr==2
+    if motionCorr==2 || motionCorr==5
         dod = hmrIntensity2OD( d );
-        addpath(genpath('homer2')); %must be in the pwd to work
-        [dod] = hmrMotionCorrectWavelet(dod,SD,1.5);
-%         if ~isempty(coords)
-%             dod = spatialPCFilter(dod, coords);
-%         end
+        if motionCorr==2
+            if ~isempty(coords)
+                dod = spatialPCFilter(dod, coords);
+            end
+        else
+            [dod] = hmrMotionCorrectWavelet(dod,SD,1.5);
+        end
         dod = hmrBandpassFilt(dod,samprate,0.008,0.2);
         ppf = [6 6];
         dconverted = hmrOD2Conc( dod, SD, ppf );
     end
     
-    if any(or(motionCorr==0,motionCorr==4))
+    if any(or(motionCorr==4,motionCorr==6))
         dfiltered=d;
         %see hmrIntensity2Conc in Homer2 documentation for parameter description
         [dconverted, ~] = hmrIntensity2Conc(dfiltered, SD, samprate, 0.008, 0.2, [6, 6]);
     end
      
-    if any(or(motionCorr==3,motionCorr==4))
+    if motionCorr==4
         dconverted = hmrMotionCorrectCbsi(dconverted,SD);
     end
     
