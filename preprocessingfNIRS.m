eea8c9010afc17d73ae7b5fbeae5802d0b52c817
abinnquist <57020313+abinnquist@users.noreply.github.com>
2022-09-25 17:47:19 -0700

    Update preprocessingfNIRS.m

--- preprocessingfNIRS.m
+++ preprocessingfNIRS.m
@@ -3,6 +3,9 @@
 %required inpaint_nans, homer2 scripts for non-snirf processing
 
 function preprocessingfNIRS(dataprefix, hyperscan, multiscan, motionCorr, numaux)
+% To run script without using command line function OR for more information  
+% see 'runNIRSPreproc' script in the main folder
+
 %inputs: 
 %       dataprefix: string. Prefix of every folder name that should be considered a
 %       data folder. E.g., 'ST' for ST_101, ST_102, etc.  
@@ -20,52 +23,47 @@ function preprocessingfNIRS(dataprefix, hyperscan, multiscan, motionCorr, numaux
 %outputs: three options of preprocessed files in a new folder in rawdir called
 %           'PreProcessedFiles', sorted by subject. 
 
-% Supplemental csv for trimming scans. In the command window after data
-% trim: 0 or 1. 0 for no CSV, 1 for CSV. Structure below 
-% Column1=subject or dyad number; 
-% column2=1st scan where to start trim; column3=1st scan length; 
-% column4=2nd scan start trim; column5=2nd scan length.
-
-%if you get 'WARNING: Some data points in d are zero...' this is ok.
-%this would normally indicate noise in our data, but since we're doing
-%motion correction before filtering, our motion correction algorithm might
-%force the data into negative numbers while still being good data. you can
-%ignore this.
-
-%DEBUGGING TIPS:
-%Note that this function is, well, funcitonal, but not 100% optimized for
-%everything that could go wrong. A couple problems I have noticed so far,
-%and how to fix them if it comes up for you:
-%   - If you get an "Index exceeds matrix dimensions" error in
-%   hmrMotionArtifact for a subject that's not the first file:
-%       Check the SD Mask structure in the .hdr of that subject to see if 
-%       it matches the channel structure of the selected probeInfo file. If
-%       the wrong probeInfo file was chosen, this will throw the error.
-%       also happens if the wrong montage was selected in recording, Simply copy-paste
-%       the correct SD Mask and ChannelDistance list into the .hdr file from a
-%       subject's .hdr file that had the correct montage.
+%Adding folders to directory if not already
 addpath(genpath("0_designOptions/")); addpath(genpath("1_extractFuncs/")); 
 addpath(genpath("3_removeNoisy/")); addpath(genpath("4_filtering/"));
 addpath(genpath("5_qualityControl/")); addpath(genpath("6_compileData/"));
 
-rawdir=uigetdir('','Choose Data Directory');
+%What device was used, NIRScout, NIRSport, or if you have SNIRF file
+supported_devices = {'NIRx-NirScout or NirSport1','NIRx-NirSport2 or .nirs file','.Snirf file (must have Homer3)'};
+[device,~] = listdlg('PromptString', 'Select acquisition device:',...
+    'SelectionMode', 'single', 'ListString', supported_devices);
 
+%Directory selection
+rawdir=uigetdir('','Choose Data Directory');
 currdir=dir(strcat(rawdir,filesep,dataprefix,'*'));
 if length(currdir)<1
     error(['ERROR: No data files found with ',dataprefix,' prefix']);
 end
 
+%If you want trim the data at all before preproc and if you want to use a csv to trim the data 
+trimTypes = {'No trim','Beginning with first trigger', 'Beginning with .csv','Begin & end with .csv'};
+[trim,~] = listdlg('PromptString', 'Do you want to trim scans?',...
+    'SelectionMode', 'single', 'ListString', trimTypes);
+if trim == 3 || trim == 4
+    [trimTs, trimPath] = uigetfile('*.csv','Choose trim time CSV');
+    trimTimes = readtable(strcat(trimPath,trimTs));
+    clear trimPath trimTs trimTypes
+else
+    trimTimes=[];
+end
+
+%Will select correct script for study design (i.e., hyperscan? multiscan?)
 if hyperscan
     if multiscan
-        preprocessHyperMulti(dataprefix, currdir, rawdir, motionCorr, numaux);
+        preprocessHyperMulti(dataprefix, currdir, rawdir, motionCorr, device, numaux, trim, trimTimes);
     else
-        preprocessHyperSingle(dataprefix, currdir, rawdir, motionCorr, numaux);
+        preprocessHyperSingle(dataprefix, currdir, rawdir, motionCorr, device, numaux, trim, trimTimes);
     end
 else
     if multiscan
-        preprocessSoloMulti(dataprefix, currdir, rawdir, motionCorr, numaux);
+        preprocessSoloMulti(dataprefix, currdir, rawdir, motionCorr, device, numaux, trim, trimTimes);
     else
-        preprocessSoloSingle(dataprefix, currdir, rawdir, motionCorr, numaux);
+        preprocessSoloSingle(dataprefix, currdir, rawdir, motionCorr, device, numaux, trim, trimTimes);
     end
 end
 
