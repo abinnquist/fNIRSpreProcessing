8ff3df706e239956b3dbdd1ae3408dba9fe8f446
abinnquist <57020313+abinnquist@users.noreply.github.com>
2022-10-01 13:38:54 -0700

    Update compilesoloNIRSdata.m

--- 6_compileData/compilesoloNIRSdata.m
+++ 6_compileData/compilesoloNIRSdata.m
@@ -6,55 +6,59 @@ function [deoxy3D,oxy3D]= compilesoloNIRSdata(preprocess_dir,dataprefix,ch_rejec
 %find all of the preprocessed folders
 currdir=dir(strcat(preprocess_dir,filesep,dataprefix,'*'));
 
-if zdim %will compile zscored data (i.e., z_oxy/z_deoxy)
-    for sc=1:numScans
-        z_oxy1=nan(1,1,2);
-        z_deoxy1=nan(1,1,2);
+for sc=1:numScans
+    z_oxyC=nan(1,1,1); z_deoxyC=nan(1,1,1);
+    tC=nan(1,1,1); sC=nan(1,1,1);
+    auxC=nan(1,1,1); 
 
-        for i=1:length(currdir)
-            subject=currdir(i).name; %define dyad
-            subfolder=dir(strcat(preprocess_dir,filesep,subject,filesep,dataprefix,'*'));
+    for i=1:length(currdir)
+        subject=currdir(i).name; %define dyad
+        subfolder=dir(strcat(preprocess_dir,filesep,subject,filesep,dataprefix,'*'));
 
-            if ~isempty(subfolder)
-                subfiles=dir(strcat(subfolder(1).folder,filesep,subfolder(sc).name,filesep,'*.mat'));
-                if ~isempty(subfiles)
-                    load(strcat(subfiles(1).folder,filesep,subfiles(ch_reject).name),'z_oxy','z_deoxy') 
+        if ~isempty(subfolder)
+            subfiles=dir(strcat(subfolder(1).folder,filesep,subfolder(sc).name,filesep,'*.mat'));
+            if ~isempty(subfiles)
+                if zdim
+                    load(strcat(subfiles(ch_reject).folder,filesep,subfiles(ch_reject).name),'z_oxy','z_deoxy','t','s','samprate','aux') 
                     [length_convo, numchans]=size(z_oxy);
                     z_deoxy1(1:length_convo,1:numchans,i)=z_deoxy(1:length_convo,:);
                     z_oxy1(1:length_convo,1:numchans,i)=z_oxy(1:length_convo,:);
-                end
-            end
-        end
-        deoxy3D(sc).name=strcat('Scan',num2str(sc));
-        deoxy3D(sc).subdata=z_deoxy1;
-      
-        oxy3D(sc).name=strcat('Scan',num2str(sc));
-        oxy3D(sc).subdata=z_oxy1;
-    end  
-else %will compile non-zscored data (i.e., oxy/deoxy)
-    for sc=1:numScans
-        oxy1=nan(1,1,2);
-        deoxy1=nan(1,1,2);
-
-        for i=1:length(currdir)
-            subject=currdir(i).name; %define dyad
-            subfolder=dir(strcat(preprocess_dir,filesep,subject,filesep,dataprefix,'*'));       
-    
-            if ~isempty(subfolder)
-                subfiles=dir(strcat(subfolder(sc).folder,filesep,subfolder(sc).name,filesep,'*.mat'));
-                if ~isempty(subfiles)
-                    load(strcat(subfiles(1).folder,filesep,subfiles(ch_reject).name),'oxy','deoxy') 
+                else
+                    load(strcat(subfiles(ch_reject).folder,filesep,subfiles(ch_reject).name),'oxy','deoxy','t','s','samprate','aux') 
                     [length_convo, numchans]=size(oxy);
-                    deoxy1(1:length_convo,1:numchans,i)=deoxy(1:length_convo,:);
-                    oxy1(1:length_convo,1:numchans,i)=oxy(1:length_convo,:);
+                    z_deoxy1(1:length_convo,1:numchans,i)=deoxy(1:length_convo,:);
+                    z_oxy1(1:length_convo,1:numchans,i)=oxy(1:length_convo,:);
+                end
+            else %for single sub & single scan
+                if zdim
+                    load(strcat(subfolder(ch_reject).folder,filesep,subfolder(ch_reject).name),'z_oxy','z_deoxy','t','s','samprate','aux') 
+                    [length_convo, numchans]=size(z_oxy);
+                    z_deoxy1(1:length_convo,1:numchans,i)=z_deoxy(1:length_convo,:);
+                    z_oxy1(1:length_convo,1:numchans,i)=z_oxy(1:length_convo,:);
+                else
+                    load(strcat(subfolder(ch_reject).folder,filesep,subfolder(ch_reject).name),'oxy','deoxy','t','s','samprate','aux') 
+                    [length_convo, numchans]=size(oxy);
+                    z_deoxy1(1:length_convo,1:numchans,i)=deoxy(1:length_convo,:);
+                    z_oxy1(1:length_convo,1:numchans,i)=oxy(1:length_convo,:);
                 end
             end
+            [lenAux,nAux]=size(aux);
+            tC(1:length_convo,1,i)=t';
+            sC(1:length_convo,1,i)=s;
+            auxC(1:lenAux,1:nAux,i)=aux(1:lenAux,:);
         end
-        deoxy3D(sc).name=strcat('Scan',num2str(sc));
-        deoxy3D(sc).subdata=deoxy1;
-        
-        oxy3D(sc).name=strcat('Scan',num2str(sc));
-        oxy3D(sc).subdata=oxy1;
     end
-end
-
+    deoxy3D(sc).name=strcat('Scan',num2str(sc));
+    deoxy3D(sc).subdata=z_deoxy1;
+    deoxy3D(sc).t=tC;
+    deoxy3D(sc).triggers=sC;
+    deoxy3D(sc).aux=auxC;
+    deoxy3D(sc).samprate=samprate;
+  
+    oxy3D(sc).name=strcat('Scan',num2str(sc));
+    oxy3D(sc).subdata=z_oxy1;
+    oxy3D(sc).t=tC;
+    oxy3D(sc).triggers=sC;
+    oxy3D(sc).aux=auxC;
+    oxy3D(sc).samprate=samprate;
+end  
