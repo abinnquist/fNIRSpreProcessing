443cef491947592775211fe117dcffac9e328949
abinnquist <57020313+abinnquist@users.noreply.github.com>
2022-12-03 16:43:26 -0800

    Update triggerCheck.m

--- helperScripts/triggerCheck.m
+++ helperScripts/triggerCheck.m
@@ -1,126 +1,161 @@
-clear
 %This will collect the trigger data for the current study to check if there
 %are any problems, inconsistencies, etc before runnning the preprocessing.
 %I often use the first trigger to trim the data so I always want to make sure it
 %is present or in the correct location before preprocessing
 
-% INPUTS
-dataprefix='0'; %Data prefix for the study
+% I would reccomend running step by step and checking that the amount of scans 
+% are correct and the names are correct. Only change the 'Inputs' section.
+clear
+
+%% INPUTS
+dataprefix='IPC'; %Data prefix for the study
+IDLength=4; % Length of ID+1 (i.e. IPC_103_rest=4); 0=ID not in scan name
 hyper=1; %If hyperscanning
 numscans=5; %Number of scans per subject
+rawdir=uigetdir('','Choose data directory');
+
+%% Step 1: Scan numbers and names
+addpath(genpath("fNIRSpreProcessing/1_extractFuncs/")); 
 
 supported_devices = {'NIRx-NirScout or NirSport1','NIRx-NirSport2 or .nirs file','.Snirf file'};
 [device,~] = listdlg('PromptString', 'Select acquisition device:',...
     'SelectionMode', 'single', 'ListString', supported_devices);
 
-rawdir=uigetdir('','Choose data directory');
 currdir=dir(strcat(rawdir,filesep,dataprefix,'*'));
 
-subjectNames={};
-scannames=cell(1,numscans);
+%Check how many scans per participant
+[scanCount, scannames] = countScans(currdir, rawdir, dataprefix, hyper, numscans);
+
+%% Step 2: Get scan names
+if hyper
+    snames = cell(width(scannames),1,1);
+    for s=1:width(scannames)
+        sc=scannames{1,s};
+        sc=sc(length(dataprefix)+IDLength+2:end);
+        snames(s,1)={sc};
+    end
+else
+    snames=cell(width(scannames),1);
+    for s=1:width(scannames)
+        sc=scannames{1,s};
+        sc=sc(length(dataprefix)+IDLength+2:end);
+        snames(s,1)={sc};
+    end
+end
+
+%% Step 3: Collect trigger info and save
 if hyper==1
-    dyadNums={};
-    triggers=nan(length(currdir),numscans,2);
-    scanLength=nan(length(currdir),numscans,2);
-    for g=1:length(currdir)
-        group=currdir(g).name;  
-        groupdir=dir(strcat(rawdir,filesep,group,filesep,dataprefix,'*'));
-    
-        for p=1:length(groupdir)
-            subjname = groupdir(p).name;
-            subjectNames=[subjectNames,subjname];
-            dyadNums=[dyadNums,group];
-            subjdir = dir(strcat(rawdir,filesep,group,filesep,subjname,filesep,dataprefix,'*'));
-    
-            for k=1:length(subjdir)
+    fprintf('\n\t Checking triggers ...\n')
+    reverseStr = '';
+    Elapsedtime = tic;
+
+    trigInfo=struct;
+
+    for k=1:numscans
+        dyadNums=cell(length(currdir),1);
+        subjectNames=cell(length(currdir),width(scanCount));
+        scanLength=nan(length(currdir),width(scanCount));
+        triggers=zeros(length(currdir),1,2);
+
+        for g=1:length(currdir)
+            group=currdir(g).name;  
+            groupdir=dir(strcat(rawdir,filesep,group,filesep,dataprefix,'*'));
+        
+            for p=1:length(groupdir)
+                subjname = groupdir(p).name;
+                subjdir = dir(strcat(rawdir,filesep,group,filesep,subjname,filesep,dataprefix,'*'));
+
+                msg = sprintf('\n\t scan %d/%d, group %d/%d, subj %d/%d ...', k,numscans,g,length(currdir),p,length(groupdir));
+                fprintf([reverseStr,msg]);
+                reverseStr = repmat(sprintf('\b'),1,length(msg)); 
+
                 scanname = subjdir(k).name;
-                scannames(1,k)={scanname};
-                scanfolder = strcat(rawdir,filesep,group,filesep,subjname,filesep,scanname);
+                scandir=dir(strcat(rawdir,filesep,group,filesep,subjname,filesep,subjdir(k).name,filesep));
+                subjectNames(g,p)={subjname};
+                dyadNums(g,p)={group};
+
+                if length(scandir) > 2
+                    scanfolder = strcat(rawdir,filesep,group,filesep,subjname,filesep,scanname);
+                    if device==1
+                        [d, sd_ind, samprate, wavelengths, s] = extractNIRxData(scanfolder);
+                    else
+                        [d, samprate, s, ~, ~, ~] = extractTechEnData(scanfolder);
+                    end
+                    lenScan=length(d);
+                    scanLength(g,p)=lenScan;
+    
+                    ssum = sum(s,2);
+                    stimmarks = find(ssum);
+    
+                    if isempty(stimmarks)
+                        triggers(g,1,p)=0;
+                    else
+                        nt=length(stimmarks);
+                        triggers(g,1:nt,p)=stimmarks;
+                    end
+                end
+            end
+        end  
+        trigInfo(k).scanname=snames{k};
+        trigInfo(k).dyad=dyadNums;
+        trigInfo(k).subNum=subjectNames;
+        trigInfo(k).trigs=triggers;   
+        trigInfo(k).scanLen=scanLength;
+    end
+    Elapsedtime = toc(Elapsedtime);
+    fprintf('\n\t Elapsed time: %g seconds\n', Elapsedtime);
+else
+    fprintf('\n\t Checking triggers ...\n')
+    reverseStr = '';
+    Elapsedtime = tic;
+
+    for k=1:numscans
+        subjectNames=cell(length(currdir),1);
+        scanLength=nan(length(currdir),1);
+        triggers=nan(length(currdir),numscans);
+
+        for p=1:length(currdir)
+            subjname = currdir(p).name;
+            subjectNames(p,1)={subjname};
+            subjdir = dir(strcat(rawdir,filesep,subjname,filesep,dataprefix,'*'));
+
+            msg = sprintf('\n\t scan %d/%d, subj %d/%d ...',k,numscans,p,length(currdir));
+            fprintf([reverseStr,msg]);
+            reverseStr = repmat(sprintf('\b'),1,length(msg)); 
+
+            scanname = subjdir(k).name;
+            scandir=dir(strcat(rawdir,filesep,subjname,filesep,subjdir(k).name,filesep));
+
+            if length(scandir) > 2
+                scanfolder = strcat(rawdir,filesep,subjname,filesep,scanname);
+
                 if device==1
                     [d, sd_ind, samprate, wavelengths, s] = extractNIRxData(scanfolder);
                 else
-                    [d, samprate, s, ~, ~, ~] = extractTechEnData(scanfolder);
-                end
+                    [d, samprate, s, SD, aux, t] = extractTechEnData(scanfolder);
+                end  
                 lenScan=length(d);
-                scanLength(g,k,p)=lenScan;
+                scanLength(p,1)=lenScan;
 
                 ssum = sum(s,2);
                 stimmarks = find(ssum);
     
                 if isempty(stimmarks)
-                    triggers(g,k,p)=0;
+                    triggers(p,k)=0;
                 else
-                    triggers(g,k,p)=stimmarks(1);
+                    nt=length(stimmarks);
+                    triggers(p,1:nt)=stimmarks;
                 end
             end
         end
+        trigInfo(k).scanname=snames{k};
+        trigInfo(k).subNum=subjectNames;
+        trigInfo(k).trigs=triggers;   
+        trigInfo(k).scanLen=scanLength; 
     end
-else
-    triggers=nan(length(currdir),numscans);
-    for p=1:length(groupdir)
-        subjname = currdir(p).name;
-        subjectNames=[subjectNames,subjname];
-        subjdir = dir(strcat(rawdir,filesep,subjname,filesep,dataprefix,'*'));
-    
-        for k=1:length(subjdir)
-            scanname = subjdir(k).name;
-            scannames(1,k)={scanname};
-            scanfolder = strcat(rawdir,filesep,subjname,filesep,scanname);
-            if device==1
-                [d, sd_ind, samprate, wavelengths, s] = extractNIRxData(scanfolder);
-            else
-                [d, samprate, s, SD, aux, t] = extractTechEnData(scanfolder);
-            end  
-            ssum = sum(s,2);
-            stimmarks = find(ssum);
-
-            if isempty(stimmarks)
-                triggers(p,k)=0;
-            else
-                triggers(p,k)=stimmarks;
-            end
-        end
-    end
+    Elapsedtime = toc(Elapsedtime);
+    fprintf('\n\t Elapsed time: %g seconds\n', Elapsedtime);
 end
 
-if hyper
-    studyInfo=array2table(cell(length(subjectNames),2));
-    studyInfo.Properties.VariableNames={'Dyad','Subject'};
-    dnames = extract(string(dyadNums),digitsPattern);
-    sjnames = extract(string(subjectNames),digitsPattern);
-    studyInfo.Dyad=dnames';
-    studyInfo.Subject=sjnames'; %If your subjects have letters, change this
-   
-    triggers=reshape(triggers,g*p,numscans); %dyads * subjects per dyad
-    scanlen=reshape(scanLength,g*p,numscans); %dyads * subjects per dyad
-else
-    studyInfo=array2table(cell(length(subjectNames),1));
-    studyInfo.Properties.VariableNames={'Subject'};
-    sjnames = extract(string(subjectNames),digitsPattern);
-    studyInfo.Subject=sjnames'; 
-end
-
-snames = extract(string(scannames),lettersPattern);
-[~, ~, thirdD]=size(snames);
-if strcmp(dataprefix,snames(1,1,1)) || thirdD > 1
-    sname=snames(1,:,2);
-end
-lenInterest=scanlen-triggers;
-
-sLenName=sname(1,:)+'_length'; %length of entire scan
-sTrigName=sname(1,:)+'_trig'; %first trigger
-sIName=sname(1,:)+'_active'; %time after first trigger
-scanlen=array2table(scanlen,"VariableNames",sLenName);
-triggers=array2table(triggers,"VariableNames",sTrigName);
-lenInterest=array2table(lenInterest,"VariableNames",sIName);
-
-triggerC=[];
-for c=1:numscans
-    triggerC=[triggerC,triggers(:,c),lenInterest(:,c),scanlen(:,c)];
-end
-triggerC=[studyInfo,triggerC];
-
-save(strcat(rawdir,filesep,'triggerCheck'),'triggerC')
+save(strcat(rawdir,filesep,'trigInfo.mat'),'trigInfo')
 clear
-
-
