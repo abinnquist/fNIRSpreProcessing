fd2457f20c78cbf511ef97c9f79d6f39760a6184
abinnquist <57020313+abinnquist@users.noreply.github.com>
2022-12-03 16:43:06 -0800

    Update extractTechEnData.m

--- 1_extractFuncs/extractTechEnData.m
+++ 1_extractFuncs/extractTechEnData.m
@@ -4,11 +4,6 @@
 
 function [d, samprate, s, SD, aux, t] = extractTechEnData(subjfolder)
     nirsfile = dir(strcat(subjfolder,'/*.nirs'));
-    loadedsubjfile = load(strcat(subjfolder,'/',nirsfile(1).name),'-mat');
-    d = loadedsubjfile.d;
-    s = loadedsubjfile.s;
-    SD = loadedsubjfile.SD;
-    aux = loadedsubjfile.aux;
-    t = loadedsubjfile.t;
+    load(strcat(subjfolder,'/',nirsfile(1).name),'-mat');
     samprate = 1/mean(diff(t(:)));
 end
