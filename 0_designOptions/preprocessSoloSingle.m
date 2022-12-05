7c862a08a0da8d4270d956808bc4d8bc41c70cf1
abinnquist <57020313+abinnquist@users.noreply.github.com>
2022-12-04 17:07:29 -0800

    Update preprocessSoloSingle.m

--- 0_designOptions/preprocessSoloSingle.m
+++ 0_designOptions/preprocessSoloSingle.m
@@ -4,9 +4,10 @@ fprintf('\n\t Preprocessing ...\n')
 reverseStr = '';
 Elapsedtime = tic;
 
-compInfo = inputdlg({'Compile  data? (0=no, 1=yes)','Number of Scans (1-n)',...
-    'Compile Z-score? (0=no, 1=yes)','Which channel rejection? (1=none, 2=noisy, or 3=noisy and uncertain)'},...
-              'Compile data info', [1 35]); 
+compInfo = inputdlg({'Run Quality Check? (0=no, 1=yes)','ID length in scan name (e.g., IPC_103_rest=4; CF005_rest=3; SNV_rest=0)?',...
+    'Compile  data? (0=no, 1=yes)','Compile Z-score? (0=no, 1=yes)',...
+    'Channel rejection? (1=none, 2=noisy, or 3=noisy & uncertain)'},...
+              'Compile data info', [1 75]);
 
 for i=1:length(currdir)
     subjname=currdir(i).name;
@@ -60,7 +61,7 @@ for i=1:length(currdir)
 
         % 2) Trim scans: no=0, trim beginnin=1, trim begin & end=2
         scanNum=i; subj=i; numScans=1;
-        [d,s,t,aux] = trimData(trim, d, s, t, subj, scanNum, numScans, trimTimes, samprate, numaux, aux);
+        [d,s,t,aux] = trimData(trim, d, s, t, subj, scanNum, numScans, trimTimes, samprate, numaux, aux, numaux);
 
         %3) identify noisy channels
         satlength = 2; %in seconds
@@ -141,16 +142,25 @@ for i=1:length(currdir)
 end
 
 preprocdir = strcat(rawdir,filesep,'PreProcessedFiles');
-qualityReport(dataprefix,0,0,{'scan'},numchannels,preprocdir);
-    
-%6) Compile data into one .mat file
-if compInfo{1,1}=='1'
-    numScans=str2num(compInfo{2,1});
-    zdim=str2num(compInfo{3,1});
-    ch_reject=str2num(compInfo{4,1});
-    [deoxy3D,oxy3D]= compilesoloNIRSdata(preprocdir,dataprefix,ch_reject,numScans,zdim);
+if compInfo{1,1}=='1' || compInfo{3,1}=='1'
+    hyperscan=0;
+    IDlength=str2num(compInfo{2,1});
+    %Gets the scan names for all subjects
+    [~, ~, snames] = countScans(currdir, preprocdir, dataprefix, hyperscan, 1, IDlength);   
 
-    save(strcat(preprocdir,filesep,dataprefix,'_compile.mat'),'oxy3D', 'deoxy3D');
+    %6.1) Quality Check
+    if compInfo{1,1}=='1'
+        qualityReport(dataprefix,1,1,numchannels,preprocdir,snames);
+    end
+
+    %6.2) Compile data into one .mat file
+    if compInfo{3,1}=='1'
+        zdim=str2num(compInfo{4,1});
+        ch_reject=str2num(compInfo{5,1});
+        [deoxy3D,oxy3D]= compilesoloNIRSdata(preprocdir,dataprefix,ch_reject,1,zdim,snames);
+    
+        save(strcat(preprocdir,filesep,dataprefix,'_compile.mat'),'oxy3D', 'deoxy3D');
+    end
 end
     
 Elapsedtime = toc(Elapsedtime);
