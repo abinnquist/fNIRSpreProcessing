112e5541ee09a68b844d9f3b0fbe10885193c967
abinnquist <57020313+abinnquist@users.noreply.github.com>
2023-06-09 17:36:51 -0700

    Delete extractTechEnData.m

--- 1_extractFuncs/extractTechEnData.m
+++
@@ -1,9 +0,0 @@
-%-----------------------------------------------
-%step 1 - subfunctions for extracting data
-%-----------------------------------------------
-
-function [d, samprate, s, SD, aux, t] = extractTechEnData(subjfolder)
-    nirsfile = dir(strcat(subjfolder,'/*.nirs'));
-    load(strcat(subjfolder,'/',nirsfile(1).name),'-mat');
-    samprate = 1/mean(diff(t(:)));
-end
