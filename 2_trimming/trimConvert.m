bf4ed9fc62b55438c075f376537ec123d7aec396
abinnquist <57020313+abinnquist@users.noreply.github.com>
2023-06-19 16:55:26 -0700

    Create trimConvert.m

---
+++ 2_trimming/trimConvert.m
@@ -0,0 +1,14 @@
+
+function trimTimes =  trimConvert(trimTs, trimPath, numscans)
+opts = detectImportOptions(strcat(trimPath,trimTs)); 
+
+trmT = table2array(readtable(strcat(trimPath,trimTs)));
+numsubs=width(trmT)/numscans;
+trimTimes=nan(height(trmT),numscans,numsubs);
+st=1;
+for s=1:numsubs
+    trimTimes(:,:,s)=trmT(:,st:st+numscans-1);
+    st=st+numscans;
+end
+
+
