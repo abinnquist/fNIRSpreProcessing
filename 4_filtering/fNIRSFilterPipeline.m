4ff861d40e96a8ce564e2ec267cc28431b5dc1ed
Ashley Binnquist <57020313+abinnquist@users.noreply.github.com>
2026-07-06 13:13:46 -0700

    Add files via upload

--- 4_filtering/fNIRSFilterPipeline.m
+++ 4_filtering/fNIRSFilterPipeline.m
@@ -8,17 +8,18 @@ function [dconverted, dnormed]= fNIRSFilterPipeline(d, SD, samprate, motionCorr,
     warning off; %sometimes hmrIntensity2Conc gives a warning we don't care about here
     %see hmrMotionArtifact in Homer2 documentation for parameter description
     
-    if any(or(motionCorr==1,motionCorr==3))
+    if contains(num2str(motionCorr),["1","3"])
         [tInc,tIncCh] = hmrMotionArtifactByChannel(d, samprate, SD, ones(length(d),1), 1, 1, 5, 2);
-        if motionCorr==1
+        if contains(num2str(motionCorr),"1")
             dfiltered = BaselineVolatilityCorrection(d, samprate, SD, tIncCh);
-        else
+        end
+        if contains(num2str(motionCorr),"3")
             [dfiltered,~,~] = hmrMotionCorrectPCA_Ch( SD, d, tInc, tIncCh, 0.9);
         end
         [dconverted, ~] = hmrIntensity2Conc(dfiltered, SD, samprate, 0.008, 0.2, [6, 6]);
     end
     
-    if motionCorr==2 || motionCorr==5 || motionCorr==6
+    if contains(num2str(motionCorr),["2","5","6"])
         dod = hmrIntensity2OD( d );
         
         [repInf(:,1),repInf(:,2)]=find(isinf(dod)); %If an Inf value shows up
@@ -32,27 +33,29 @@ function [dconverted, dnormed]= fNIRSFilterPipeline(d, SD, samprate, motionCorr,
             end
         end
 
-        if motionCorr==2
+        if contains(num2str(motionCorr),"2")
             if ~isempty(coords)
                 dod = spatialPCFilter(dod, coords);
             end
-        elseif motionCorr==5
-            [dod] = hmrMotionCorrectWavelet(dod,SD,1.5);
-        elseif motionCorr==6 %In progress
+        end
+        if contains(num2str(motionCorr),"6") % Short-channel Regression
             [dod, ~] = hmrSSR(dod, SD, 0, t);
+        end
+        if contains(num2str(motionCorr),"5") % Wavelet 
+            [dod] = hmrMotionCorrectWavelet(dod,SD,1.5);
         end
         dod = hmrBandpassFilt(dod,samprate,0.008,0.2);
         ppf = [6 6];
         dconverted = hmrOD2Conc( dod, SD, ppf );
     end
     
-    if any(or(motionCorr==4,motionCorr==7))
+    if contains(num2str(motionCorr),["4","7"])
         dfiltered=d;
         %see hmrIntensity2Conc in Homer2 documentation for parameter description
         [dconverted, ~] = hmrIntensity2Conc(dfiltered, SD, samprate, 0.008, 0.2, [6, 6]);
     end
      
-    if motionCorr==4
+    if contains(num2str(motionCorr),"4")
         dconverted = hmrMotionCorrectCbsi(dconverted,SD);
     end
     
